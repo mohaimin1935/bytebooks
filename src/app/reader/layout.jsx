17772c7e742eb4dbb599c8c61f9e576b19157667
mohaimin1935 <mi.mohaimin@gmail.com>
2024-01-24 23:12:14 +0600

    production error fix

--- src/app/reader/layout.jsx
+++ src/app/reader/layout.jsx
@@ -4,23 +4,29 @@ import TopBar from "../ui/common/TopBar";
 
 const ReaderLayout = ({ children }) => {
   return (
-    <div class="flex h-screen bg1">
-      {/* left panel */}
-      <div class="w-[300px] bg2">
-        <ReaderLeft active={"for-you"} />
-      </div>
+    <html>
+      <body>
+        <div class="flex h-screen bg1">
+          {/* left panel */}
+          <div class="w-[300px] bg2">
+            <ReaderLeft active={"for-you"} />
+          </div>
 
-      {/* right */}
-      <div class="flex-1 flex flex-col overflow-hidden px-16">
-        {/* top navbar */}
-        <div class="bg-transparent">
-          <TopBar role={"reader"} />
+          {/* right */}
+          <div class="flex-1 flex flex-col overflow-hidden px-16">
+            {/* top navbar */}
+            <div class="bg-transparent">
+              <TopBar role={"reader"} />
+            </div>
+
+            {/* main content */}
+            <div class="flex-1 pr-16 -mr-16 overflow-y-auto py-12">
+              {children}
+            </div>
+          </div>
         </div>
-
-        {/* main content */}
-        <div class="flex-1 pr-16 -mr-16 overflow-y-auto py-12">{children}</div>
-      </div>
-    </div>
+      </body>
+    </html>
   );
 };
 
