75807b95a88c560dafb3098292c7c59b7b17b817
Zulkar Naim <zulkarnaim618@gmail.com>
2024-01-25 17:16:03 +0600

    added users/userId get request

---
+++ src/app/api/users/[userId]/route.js
@@ -0,0 +1,35 @@
+import { creatorOnlyFailed } from "@/middleware/authorization";
+import prisma from "@/utils/connect";
+import { NextResponse } from "next/server";
+import validateMandatoryFields  from "@/middleware/mandatoryFieldList";
+
+// need to add authentication check and other error codes
+export const GET = async (req,{params}) => {
+    //console.log(params.userId);
+  try {
+    const user = await prisma.User.findUnique({
+        where: {
+            id: params.userId,
+        },
+        select: {
+            id: true,
+            name: true,
+            email: true,
+            image: true,
+            role: true,     // need to add country and xp
+        }
+    });
+    //console.log(user)
+    return NextResponse.json(user);
+  } catch (err) {
+    console.log(err);
+
+    
+
+    return NextResponse.json(
+      { message: "Something went wrong" },
+      { status: 500 }
+    );
+  }
+};
+
