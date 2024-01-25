6845956c233e0c4225fb97815a7c5923504fc219
Zulkar Naim <zulkarnaim618@gmail.com>
2024-01-25 18:16:14 +0600

    added update user details route

--- src/app/api/users/[userId]/route.js
+++ src/app/api/users/[userId]/route.js
@@ -35,3 +35,39 @@ export const GET = async (req,{params}) => {
   }
 };
 
+// need to add authentication check and other error codes
+export const PUT = async (req,{params}) => {
+    //console.log(params.userId);
+  try {
+    const body = await req.json();
+    const user = await prisma.User.update({
+        where: {
+            id: params.userId,
+        },
+        data: {
+            name: body.name,
+            email: body.email,
+            image: body.image,
+            role: body.role,    // need to add country and xp
+        },
+        select: {
+            id: true,
+            name: true,
+            email: true,
+            image: true,
+            role: true,     // need to add country and xp
+        }
+    });
+    console.log(user)
+    return NextResponse.json(user);
+  } catch (err) {
+    console.log(err);
+
+    
+
+    return NextResponse.json(
+      { message: "Something went wrong" },
+      { status: 500 }
+    );
+  }
+};
