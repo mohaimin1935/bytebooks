5560e4f22d3b39a241605a193389a62f283ba291
Md Shafayatul Haque <shafayat@Mds-MacBook-Air-2.local>
2024-02-09 01:43:42 +0600

    kk

--- src/app/api/users/[userId]/books/[bookId]/route.js
+++ src/app/api/users/[userId]/books/[bookId]/route.js
@@ -9,7 +9,7 @@ export const POST = async (req, { params }) => {
 
   // Adjusting to the updated model fields
   const body = await req.json(); 
-  const { status, chapterId, byteId, audioTimeStampChapter, audioTimeStampBytes, notes, rating } = body;
+  const { status, chapterId, byteId, audioTimeStampChapter, audioTimeStampBytes, notes, rating ,isBookmarked } = body;
   console.log(userId, bookId, status, chapterId, byteId, audioTimeStampChapter, audioTimeStampBytes, notes, rating);
   if (!userId || !bookId || !status) {
     
@@ -41,6 +41,7 @@ export const POST = async (req, { params }) => {
           notes,
           rating,
           updatedAt: new Date(), // Optionally ensure updatedAt is explicitly set
+          isBookmarked,
         },
       });
     } else {
@@ -56,6 +57,7 @@ export const POST = async (req, { params }) => {
           audioTimeStampBytes,   // Updated to reflect model changes
           notes,
           rating,
+          isBookmarked,
         },
       });
     }
@@ -65,6 +67,41 @@ export const POST = async (req, { params }) => {
     console.error('Request error:', error);
     // Ensure an error response is returned
     return new NextResponse(JSON.stringify({ error: 'Error creating or updating BookUser entry', errorMessage: error.message }), { status: 500 });
+  }
+};
+
+export const GET = async (req, { params }) => {
+  const { userId, bookId } = params;
+
+  if (!userId || !bookId) {
+    // If userId or bookId are missing, return an error response
+    return new NextResponse(JSON.stringify({ error: 'Missing userId or bookId in parameters' }), { status: 400 });
+  }
+
+  try {
+    // Attempt to find the BookUser entry based on userId and bookId
+    const bookUser = await prisma.bookUser.findUnique({
+      where: {
+        userId_bookId: { userId, bookId }, // Composite key
+      },
+      // Optionally include related models like chapter or byte if needed
+      include: {
+        chapter: true,
+        byte: true,
+      },
+    });
+
+    if (!bookUser) {
+      // If no entry is found, return a not found response
+      return new NextResponse(JSON.stringify({ error: 'BookUser entry not found' }), { status: 404 });
+    }
+
+    // If an entry is found, return it
+    return new NextResponse(JSON.stringify(bookUser), { status: 200 });
+  } catch (error) {
+    console.error('Request error:', error);
+    // Ensure an error response is returned in case of an exception
+    return new NextResponse(JSON.stringify({ error: 'Error fetching BookUser entry', errorMessage: error.message }), { status: 500 });
   }
 };
 
