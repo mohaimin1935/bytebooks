e8e218546c3f293f0f93a77d038adf7d45f30a13
Zulkar Naim <zulkarnaim618@gmail.com>
2024-02-09 01:11:11 +0600

    user/userid/books get done

--- src/app/api/users/[userId]/books/route.js
+++ src/app/api/users/[userId]/books/route.js
@@ -1,7 +1,7 @@
 // file-status: need to work
 
 
-import { creatorOnlyFailed } from "@/middleware/authorization";
+import { selfValidationOnlyFailed } from "@/middleware/authorization";
 import prisma from "@/utils/connect";
 import { NextResponse } from "next/server";
 import validateMandatoryFields  from "@/middleware/mandatoryFieldList";
@@ -28,27 +28,60 @@ export const GET = async (req,{params}) => {
         //if no count then default count 20
         filter.count = 20;
         if (searchParams.has('count')) {
-            filter.count = searchParams.get('count');
+            filter.count = parseInt(searchParams.get('count'),10);
         }
         //if no page index default page index 0
         filter.page = 0;
         if (searchParams.has('page')) {
-            filter.page = searchParams.get('page');
+            filter.page = parseInt(searchParams.get('page'),10);
         }
         
         // Add more filters as needed
         console.log(filter);
+        
+        //need to fix logic #check
+        let results=[];
+        if (filter.type==="latest") {
 
-        const results = await prisma.BookInfo.findMany({
+        }
+        else if (filter.type==="continue") {
+
+        }
+        else if (filter.type==="trending") {
+
+        }
+        else if (filter.type==="recommended") {
+
+        }
+        results = await prisma.BookUser.findMany({
             skip: filter.count*filter.page,
             take: filter.count,
             where: {
-              userId: params.userId,
+                userId: params.userId,
             },
-            
+            // include: {
+            //     book: true,
+            // }
         });
         console.log(results);
-        return NextResponse.json(results);
+        let res = [];
+        for (let i=0;i<results.length;i++) {
+            res.push(results[i].bookId);
+        }
+        console.log(res);
+        let books = await prisma.bookInfo.findMany({
+            where: {
+                id: {
+                    in: res,
+                }
+            },
+            include: {
+              authors: { include: { author: true }},
+              genres: { include: { genre: true }},
+              // Include other relations as needed
+            },
+        });
+        return NextResponse.json(books);
   } catch (err) {
     console.log(err);
 
