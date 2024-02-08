f545ec45832bd5f55a4e743e0fe1d64ee21feed1
Zulkar Naim <zulkarnaim618@gmail.com>
2024-02-09 02:00:22 +0600

    added highlights route

---
+++ src/app/api/highlights/route.js
@@ -0,0 +1,33 @@
+import { selfValidationOnlyFailed } from "@/middleware/authorization";
+import prisma from "@/utils/connect";
+import { NextResponse } from "next/server";
+
+// create a new highlight
+export const POST = async (req) => {
+//   const authError = await selfValidationOnlyFailed(params.userId);
+//   if (authError) {
+//     return authError;
+//   }
+  
+
+  try {
+
+    const body = await req.json();
+
+    console.log(body);
+    
+    const res = await prisma.Highlights.create({ data: body });
+    console.log(res);
+
+    return NextResponse.json(res);
+  } catch (err) {
+    console.log(err);
+
+    return NextResponse.json(
+      { message: "Something went wrong" },
+      { status: 500 }
+    );
+  }
+};
+
+
