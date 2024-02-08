dcdf7df3728e04e8066c5638f6f2828a48011c8f
Md Shafayatul Haque <shafayat@Mds-MacBook-Air-2.local>
2024-02-09 02:59:48 +0600

    added userLogin Dates

--- src/app/api/users/[userId]/streak/route.js
+++ src/app/api/users/[userId]/streak/route.js
@@ -15,8 +15,30 @@ export const POST = async (req, { params }) => {
 
     const now = new Date();
     const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+    
     const yesterday = new Date(today);
     yesterday.setDate(yesterday.getDate()-1);
+
+
+    // Check if a login record already exists for today
+    const todayLogin = await prisma.userLogin.findFirst({
+      where: {
+        userId: userId,
+        loginDate: today,
+      },
+    });
+
+    // If not, create a new login record for today
+    if (!todayLogin) {
+      await prisma.userLogin.create({
+        data: {
+          userId: userId,
+          loginDate: today,
+        },
+      });
+    }
+
+
 
     let newStreak = user.loginStreak;
     const lastLoginDate = user.lastLoginDate ? new Date(user.lastLoginDate) : null;
@@ -44,3 +66,37 @@ export const POST = async (req, { params }) => {
     return new NextResponse(JSON.stringify({ error: 'Internal server error', errorMessage: error.message }), { status: 500 });
   }
 };
+
+
+
+export const GET = async (req, { params }) => {
+  const { userId } = params;
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+    });
+
+    if (!user) {
+      return new NextResponse(JSON.stringify({ error: 'User not found' }), { status: 404 });
+    }
+
+    const loginStreak = user.loginStreak;
+    const loginDates = await prisma.userLogin.findMany({
+      where: { userId: userId },
+      orderBy: { loginDate: 'desc' },
+      select: { loginDate: true },
+    });
+
+    const formattedLoginDates = loginDates.map(ld => ld.loginDate.toISOString().split('T')[0]);
+
+    return new NextResponse(JSON.stringify({
+      streak: loginStreak,
+      loginDates: formattedLoginDates,
+    }), { status: 200 });
+  } catch (error) {
+    console.error('Request error:', error);
+    return new NextResponse(JSON.stringify({ error: 'Internal server error', errorMessage: error.message }), { status: 500 });
+  }
+};
+
