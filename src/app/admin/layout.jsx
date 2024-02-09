9c8db65e4395aa783da19ad49b07475e74b0b781
Zulkar Naim <zulkarnaim618@gmail.com>
2024-02-09 18:48:53 +0600

    added admin/home skeleton

---
+++ src/app/admin/layout.jsx
@@ -0,0 +1,45 @@
+"use client";
+
+import React, { useEffect } from "react";
+import AdminLeft from "../ui/admin/AdminLeft";
+import TopBar from "../ui/common/TopBar";
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+
+const AdminLayout = ({ children }) => {
+  const { data, status } = useSession();
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.push("/login");
+    }
+  }, [status]);
+
+  if (status === "authenticated")
+    return (
+      <div className="flex h-screen bg1">
+        {/* left panel */}
+        <div className="w-[300px] bg2">
+          <AdminLeft active={"for-you"} />
+        </div>
+
+        {/* right */}
+        <div className="flex-1 flex flex-col overflow-hidden px-16">
+          {/* top navbar */}
+          <div className="bg-transparent">
+            {/* <TopBar role={"creator"} /> */}
+          </div>
+
+          {/* main content */}
+          <div className="flex-1 -mr-16 overflow-y-auto py-12">
+            {/* <div className="pr-16">{children}</div> */}
+          </div>
+        </div>
+      </div>
+    );
+  else return <></>;
+};
+
+export default AdminLayout;
---
+++ src/app/ui/admin/AdminLeft.jsx
@@ -0,0 +1,35 @@
+"use client";
+
+import React from "react";
+import LeftPanel from "../common/LeftPanel";
+import { FiBookOpen, FiDatabase, FiHome, FiSettings } from "react-icons/fi";
+import { IoAnalyticsOutline } from "react-icons/io5";
+
+const AdminLeft = ({ active }) => {
+  return <LeftPanel options={options} active={active} />;
+};
+
+const options = [
+  {
+    name: "Home",
+    link: "/admin/home",
+    icon: () => <FiHome />,
+  },
+  {
+    name: "Analytics",
+    link: "/admin/analytics",
+    icon: () => <IoAnalyticsOutline />,
+  },
+  {
+    name: "Data Manager",
+    link: "/admin/data-manager",
+    icon: () => <FiDatabase />,
+  },
+  {
+    name: "Settings",
+    link: "/admin/settings",
+    icon: () => <FiSettings />,
+  },
+];
+
+export default AdminLeft;
