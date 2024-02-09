2b337a3f3899d20d3e103d9734e6a7fcc0e2c03c
Zulkar Naim <zulkarnaim618@gmail.com>
2024-02-09 19:23:48 +0600

    added admin/data-manager & user table in data manager

---
+++ src/app/admin/data-manager/page.jsx
@@ -0,0 +1,68 @@
+"use client";
+
+import UserList from "@/app/ui/admin/UserList";
+import AuthorList from "@/app/ui/author/AuthorList";
+import GenreList from "@/app/ui/author/GenreList";
+import TagList from "@/app/ui/author/TagList";
+import { cn } from "@/utils/cn";
+import React, { useState } from "react";
+
+const AdminDataManager = () => {
+  const [active, setActive] = useState("author");
+
+  const changeTab = (tab) => {
+    setActive(tab);
+  };
+
+  return (
+    <div>
+      <div className="border-b-2 border-bkg-2 flex items-center justify-start gap-x-6 mb-8">
+        <div
+          className={cn(
+            "px-4 cursor-pointer border-b-4 border-transparent",
+            active === "user" && "border-check"
+          )}
+          onClick={() => changeTab("user")}
+        >
+          User
+        </div>
+        <div
+          className={cn(
+            "px-4 cursor-pointer border-b-4 border-transparent",
+            active === "author" && "border-check"
+          )}
+          onClick={() => changeTab("author")}
+        >
+          Author
+        </div>
+        <div
+          className={cn(
+            "px-4 cursor-pointer border-b-4 border-transparent",
+            active === "genre" && "border-check"
+          )}
+          onClick={() => changeTab("genre")}
+        >
+          Genre
+        </div>
+        <div
+          className={cn(
+            "px-4 cursor-pointer border-b-4 border-transparent",
+            active === "tag" && "border-check"
+          )}
+          onClick={() => changeTab("tag")}
+        >
+          Tag
+        </div>
+      </div>
+      {active === "user" && <UserList />}
+
+      {active === "author" && <AuthorList />}
+
+      {active === "tag" && <TagList />}
+
+      {active === "genre" && <GenreList />}
+    </div>
+  );
+};
+
+export default AdminDataManager;
---
+++ src/app/ui/admin/UserList.jsx
@@ -0,0 +1,27 @@
+import { fetcher } from "@/utils/util";
+import React from "react";
+import useSWR from "swr";
+import Loader from "../common/Loader";
+import DataTable from "../common/DataTable";
+
+const UserList = () => {
+  const { data, isLoading } = useSWR("/api/author", fetcher);
+  if (isLoading)
+    return (
+      <div className="mt-16">
+        <Loader />
+      </div>
+    );
+
+  return (
+    <DataTable
+      data={data}
+      hasImage
+      defaultImage={"/author.png"}
+      deleteApi="/api/author"
+      type={"author"}
+    />
+  );
+};
+
+export default UserList;
