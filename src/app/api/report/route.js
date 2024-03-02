2ccca12d22086f84e814be3b49a5a85cde9d172e
Md Shafayatul Haque <shafayat@Mds-MacBook-Air-2.local>
2024-03-02 16:26:29 +0600

    added report get parameters

--- src/app/api/report/route.js
+++ src/app/api/report/route.js
@@ -1,19 +1,24 @@
 // file-status: need to work
 
-
-import { adminOnlyFailed, authenticatedOnlyFailed, creatorOnlyFailed, selfValidationOnlyFailed } from "@/middleware/authorization";
+import {
+  adminOnlyFailed,
+  authenticatedOnlyFailed,
+  creatorOnlyFailed,
+  selfValidationOnlyFailed,
+} from "@/middleware/authorization";
 import prisma from "@/utils/connect";
 import { NextResponse } from "next/server";
-import validateMandatoryFields  from "@/middleware/mandatoryFieldList";
+import validateMandatoryFields from "@/middleware/mandatoryFieldList";
 
+//isToday and isPending
 // need to check error condition
-export const GET = async (req,{params}) => {
-    // const authError = await adminOnlyFailed();
-    // if (authError) {
-    //     return authError;
-    // }
+export const GET = async (req, { params }) => {
+  // const authError = await adminOnlyFailed();
+  // if (authError) {
+  //     return authError;
+  // }
 
-    /*
+  /*
         example response body
         [
             {
@@ -28,28 +33,52 @@ export const GET = async (req,{params}) => {
         ]
 
     */
-    
 
   try {
+    const url = req.nextUrl;
+    const searchParams = url.searchParams;
+
+    const isToday = searchParams.get("isToday") === "true";
+    const isPending = searchParams.get("isPending") === "true";
+
+    console.log(isToday, isPending);
+
+    let whereClause = {};
+
+    if (isPending) {
+      whereClause.status = "pending";
+    } else {
+      whereClause.status = {
+        not: "pending",
+      };
+    }
+
+    if (isToday) {
+      whereClause.updatedAt = {
+        gte: new Date(new Date().setHours(0, 0, 0, 0)),
+        lt: new Date(new Date().setHours(23, 59, 59, 999)),
+      };
+    }
+    console.log(whereClause);
+
     const res = await prisma.ContentReport.findMany({
-        where: {
-            status: "pending",
-        },
-        select: {
-            id: true,
-            userId: true,
-            bookId: true,
-            chapterId: true,
-            byteId: true,
-            comment: true,
-            status: true,
-        },
+      //   where: {
+      //     status: "pending",
+      //   },
+      where: whereClause,
+      select: {
+        id: true,
+        userId: true,
+        bookId: true,
+        chapterId: true,
+        byteId: true,
+        comment: true,
+        status: true,
+      },
     });
     return NextResponse.json(res);
   } catch (err) {
     console.log(err);
-
-    
 
     return NextResponse.json(
       { message: "Something went wrong" },
@@ -59,15 +88,14 @@ export const GET = async (req,{params}) => {
 };
 
 // need to check error condition
-export const POST = async (req,{params}) => {
+export const POST = async (req, { params }) => {
+  //   const authError = await authenticatedOnlyFailed();
+  //   if (authError) {
+  //     return authError;
+  //   }
 
-    //   const authError = await authenticatedOnlyFailed();
-    //   if (authError) {
-    //     return authError;
-    //   }
-        
-    try {
-        /*
+  try {
+    /*
             example request body, optional fields are marked with *
             {
                 "userId":,
@@ -78,31 +106,27 @@ export const POST = async (req,{params}) => {
             }
 
         */
-        const body = await req.json();
-        console.log(body);
-        const res = await prisma.ContentReport.create({
-            data: body,
-            select: {
-                id: true,
-                userId: true,
-                bookId: true,
-                chapterId: true,
-                byteId: true,
-                comment: true,
-                status: true,
-            },
-        });
-        return NextResponse.json(res);
-    } catch (err) {
-        
-        console.log(err);
+    const body = await req.json();
+    console.log(body);
+    const res = await prisma.ContentReport.create({
+      data: body,
+      select: {
+        id: true,
+        userId: true,
+        bookId: true,
+        chapterId: true,
+        byteId: true,
+        comment: true,
+        status: true,
+      },
+    });
+    return NextResponse.json(res);
+  } catch (err) {
+    console.log(err);
 
-        return NextResponse.json(
-            { message: "Something went wrong" },
-            { status: 500 }
-        );
-    }
-
+    return NextResponse.json(
+      { message: "Something went wrong" },
+      { status: 500 }
+    );
+  }
 };
-
-
